e84103cc8c932b5e8f2de40c6ae4e3bf8071ba12
Donyo Donev <donevdonyo39@gmail.com>
2021-09-17 23:50:42 +0300

    providing title to articles, with ramda plug in

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,13 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+    // This block will be executed once the page is loaded and ready
 
-  const arrayToPluck = [
-    { name: "John", class: "is-primary" },
-    { age: 23, class: "is-warning" },
-    { job: "programmer", class: "is-danger" },
-  ];
-  const articles = document.querySelectorAll("article");
+    const arrayToPluck = [
+        { name: "John", class: "is-primary" },
+        { age: 23, class: "is-warning" },
+        { job: "programmer", class: "is-danger" },
+    ];
+    const articles = document.querySelectorAll("article");
+    R.pluck(arrayToPluck);
 });
